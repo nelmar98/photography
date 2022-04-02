fb2b153f45102f176b8555177f25489d8501e0f0
Nel Mar <nelmaratanoza98@gmail.com>
2022-04-02 15:37:22 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,1 @@
+document.addEventListener("click",function(e){if(e.target.classList.contains("photo-item")){const src=e.target.getAttribute("src");document.querySelector(".modal-img").src=src;const myModal=new bootstrap.Modal(document.getElementById('photo-modal'));myModal.show()}})
